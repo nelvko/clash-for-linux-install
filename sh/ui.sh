377d97da377432da3208005ebf2764c1aafeb7bd
mojospy <76961348+coolapker@users.noreply.github.com>
2024-03-13 03:33:17 +0800

    Update ui.sh

--- sh/ui.sh
+++ sh/ui.sh
@@ -1,4 +1,8 @@
 ip=$(curl -s ifconfig.cc | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
 
-# 输出变量中的IP地址
-echo "clash面板地址：http://$ip:9090/ui"
+cat << EOF
+clash 面板UI:
+    ● 开放9090端口后使用
+    ● 地址1：http://$ip:9090/ui
+    ● 地址2：https://clash.razord.top
+EOF
