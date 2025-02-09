f319cb3bd7552028797b8ba3565cb9ba8ae855b4
libm <nelvko@qq.com>
2025-02-09 21:49:17 +0800

    fix: optimize uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -3,18 +3,17 @@
 . script/common.sh
 . script/clashctl.sh
 
-# _valid_env
+_valid_env
 
 [ ! -d "$CLASH_BASE_DIR" ] && echo "😾 未安装或已卸载,开始自动清理相关配置..."
 
-clashoff >/dev/null 2>&1
+clashoff >&/dev/null
 
-systemctl disable clash >/dev/null 2>&1
+systemctl disable clash >&/dev/null
 rm -f /etc/systemd/system/clash.service
 systemctl daemon-reload
-rm -rf "$YQ_BIN"
 rm -rf "$CLASH_BASE_DIR"
-sed -i '/clashupdate/d' "$CLASH_CRON_TAB"
+sed -i '/clashupdate/d' "$CLASH_CRON_TAB" >&/dev/null
 echo '😼 已卸载，相关配置已清除'
 # 未 export 的变量和函数不会被继承
-sed -i '/clashctl.sh/d' "$BASHRC" && exec bash
+sed -i '/clashctl.sh/d' "$BASHRC" >&/dev/null && exec bash
