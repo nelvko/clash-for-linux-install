88f2dd6758264905b08f3937e241ea726711b07f
libm <nelvko@qq.com>
2025-12-16 11:44:39 +0800

    fix

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,6 @@
 
 . scripts/cmd/clashctl.sh
 . scripts/preflight.sh
-. "$CLASH_CMD_DIR/clashctl.sh" 2>/dev/null
 
 clashoff >&/dev/null
 
@@ -11,7 +10,7 @@ _revoke_rc
 
 command -v crontab >&/dev/null && crontab -l | grep -v "clashsub" | crontab -
 
-rm -rf "$CLASH_BASE_DIR" >&/dev/null || _error_quit '请使用 sudo 执行'
+/usr/bin/rm -r "$CLASH_BASE_DIR"
 
 _okcat '✨' '已卸载，相关配置已清除'
 _quit
