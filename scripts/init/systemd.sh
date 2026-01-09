9f5ccd5a6f80aa8dfde038782c4a3964abdc34bb
libm <nelvko@qq.com>
2026-01-09 10:51:15 +0800

    perf: perfect the systemd config file, more robust
    Fix #347

--- scripts/init/systemd.sh
+++ scripts/init/systemd.sh
@@ -1,8 +1,13 @@
 [Unit]
 Description=placeholder_kernel_desc
+After=network.target NetworkManager.service systemd-networkd.service iwd.service
 
 [Service]
 Type=simple
+LimitNPROC=500
+LimitNOFILE=1000000
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME CAP_SYS_PTRACE CAP_DAC_READ_SEARCH CAP_DAC_OVERRIDE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_TIME CAP_SYS_PTRACE CAP_DAC_READ_SEARCH CAP_DAC_OVERRIDE
 Restart=always
 ExecStartPre=/usr/bin/sleep 1s
 ExecStart=placeholder_cmd_full
